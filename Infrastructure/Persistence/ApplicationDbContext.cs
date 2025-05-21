05fbf5097dd6a94801c4826918e7b4b3e2f96b34
Bijayash Chitrakar <36729904+HECATE1997@users.noreply.github.com>
2025-05-21 17:34:39 +0545

    database added
    added database

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<BlogPost> BlogPosts => Set<BlogPost>();
+        public DbSet<Comment> Comments => Set<Comment>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.BlogPosts)
+                .WithOne(p => p.Author)
+                .HasForeignKey(p => p.AuthorId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Comments)
+                .WithOne(c => c.Author)
+                .HasForeignKey(c => c.AuthorId);
+
+            modelBuilder.Entity<BlogPost>()
+                .HasMany(p => p.Comments)
+                .WithOne(c => c.Post)
+                .HasForeignKey(c => c.PostId);
+        }
+    }
+}
